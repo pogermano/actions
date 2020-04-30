419de88175a8c9ee90e6d4c7edd6e8c7cef13c4a
HAROLDO B PASSOS <haroldo@infinitusweb.com.br>
2020-04-30 14:14:13 -0300

    ToolBars In LIne Grid Scripcase
    Barra de Ferramentas na linha da Grid do Scriptcase

---
+++ IWActions.class.php
@@ -0,0 +1,11 @@
+<?php
+/* Classe iwActions
+	Classe para criação de botões de ação nas linhas de uma Grid Scriptcase
+	Autor: Haroldo B Passos / InfinitusWeb
+	Copyrirght: 2020 / 2020 Versão: 1.01.001 
+	
+	*/
+
+class IWActions
+{
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# IWPdf
+Classe PHP de apoio a Criação de Barra de ferramentas na linha da Grid Scriptcase<b>TCPDF</b>
+
+<b>Versão 1.01.001</b>
