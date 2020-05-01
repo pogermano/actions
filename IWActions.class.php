e6ca3bb2bbdeda31560de63c27dd9f38b0db6dcc
HAROLDO B PASSOS <haroldo@infinitusweb.com.br>
2020-05-01 16:34:35 -0300

    Inplementado Intepretação de imagens do repositório de Imagens do gerenciador de imagens do Scriptcase

--- IWActions.class.php
+++ IWActions.class.php
@@ -8,4 +8,55 @@
 
 class IWActions
 {
+	private $lineSeq;
+
+	private $item = [];
+	private $id = 0;
+	private $scImage = NULL;
+	private $image = Null;
+	private $fontAweSome = NULL;
+	private $text = NULL;
+	private $toolBar = [];
+	public $condition = FALSE;
+	public $imageWidth = 20;
+
+	function __construct($lineSeq)
+	{
+		$this->lineSeq = $lineSeq;
+	}
+
+	public function setContition($condition)
+	{
+		$this->condition = $condition;
+	}
+
+	public function setSCImage($imgTrue, $categTrue = 'grp,img', $imgFalse = NULL, $categFalse = 'grp,img')
+	{
+		//$categ = ['sys' => 'sys__NM__', 'prj' => 'grp__NM__ ', 'usr' => 'usr__NM__'];
+		//$type =  ['bg' => 'sys__NM__', 'prj' => 'grp__NM__ ', 'usr' => 'usr__NM__'];
+		$categ = explode(',', $categTrue);
+		$imgTrue = strtoupper($categ[0]) . '__NM__' . strtoupper($categ[1]) . '__NM__' . $imgTrue;
+		$tagImg = "<img src='../../../../img/$imgTrue' width={$this->imageWidth}/>";
+		if (!is_null($imgFalse)) {
+			$categ = explode(',', $categFalse);
+			$imgFalse = strtoupper($categ[0]) . '__NM__' . strtoupper($categ[1]) . '__NM__' . $imgFalse;
+			$tagImg = [$imgTrue, "<img src='../../../../img/$imgFalse' width={$this->imageWidth}/>"];
+		}
+
+		$this->item = $tagImg;
+		$this->image = Null;
+		$this->fontAweSome = NULL;
+		$this->text = NULL;
+	}
+
+	public function close()
+	{
+		$this->tollBar[$this->id] = $this->item;
+		$this->item = NULL;
+		$this->image = Null;
+		$this->fontAweSome = NULL;
+		$this->text = NULL;
+		$this->scImage = NULL;
+		$this->id++;
+	}
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# IWPdf
+# IWActions
 Classe PHP de apoio a Criação de Barra de ferramentas na linha da Grid Scriptcase<b>TCPDF</b>
 
 <b>Versão 1.01.001</b>
