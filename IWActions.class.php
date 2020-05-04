93f7170b7ff4bddf676927688a8d6241b192976a
HAROLDO B PASSOS <haroldo@infinitusweb.com.br>
2020-05-04 18:36:50 -0300

    Otimização de Código

--- IWActions.class.php
+++ IWActions.class.php
@@ -12,14 +12,11 @@ class IWActions
 
 	private $item = [];
 	private $id = 0;
-	private $scImage = NULL;
-	private $image = Null;
-	private $fontAweSome = NULL;
-	private $text = NULL;
-	private $toolBar = [];
 	private $condition = TRUE;
 	private $imageWidth = 20;
 	private $itemSpace = '1px';
+	private $toolBar = [];
+
 
 	function __construct($lineSeq)
 	{
@@ -33,20 +30,28 @@ class IWActions
 
 	public function setSCImage($imgTrue, $categTrue = 'grp,img', $imgFalse = NULL, $categFalse = 'grp,img')
 	{
-		//$categ = ['sys' => 'sys__NM__', 'prj' => 'grp__NM__ ', 'usr' => 'usr__NM__'];
-		//$type =  ['bg' => 'sys__NM__', 'prj' => 'grp__NM__ ', 'usr' => 'usr__NM__'];
 		$categ = explode(',', $categTrue);
-		$imgTrue = strtoupper($categ[0]) . '__NM__' . strtoupper($categ[1]) . '__NM__' . $imgTrue;
-		$this->item[0] = "<img src='../../../../img/$imgTrue' width={$this->imageWidth}/>";
+		$categ[0] =  strtolower($categ[0]) == 'prj' ? 'grp' : strtolower($categ[0]);
+		$imgTrue = $categ[0] . '__NM__' . strtolower($categ[1]) . '__NM__' . $imgTrue;
+		$this->item[0] = "<img src='../_lib/img/$imgTrue' width='{$this->imageWidth}px' />";
 		if (!is_null($imgFalse)) {
 			$categ = explode(',', $categFalse);
-			$imgFalse = strtoupper($categ[0]) . '__NM__' . strtoupper($categ[1]) . '__NM__' . $imgFalse;
-			$this->item[1] = "<img src='../../../../img/$imgFalse' width={$this->imageWidth}/>";
+			$categ[0] =  strtolower($categ[0]) == 'prj' ? 'grp' : strtolower($categ[0]);
+			$imgFalse = $categ[0] . '__NM__' . strtolower($categ[1]) . '__NM__' . $imgFalse;
+			$this->item[1] = "<img src='../_lib/img/$imgFalse' width='{$this->imageWidth}px' />";
 		}
+	}
 
-		$this->image = Null;
-		$this->fontAweSome = NULL;
-		$this->text = NULL;
+	public function setImage($imageTrue, $imageFalse = '', $path = 'grp')
+	{
+		$path = strtolower($path) == 'prj' ? 'grp' : strtolower($path);
+		$this->item[0] = "<img src='../_lib/libraries/$path/actions/img/$imageTrue'  width='{$this->imageWidth}px' />";
+		$this->item[1] = "<img src='../_lib/libraries/$path/actions/img/$imageFalse' width='{$this->imageWidth}px' />";
+	}
+
+	public function setAwesome()
+	{
+		echo "setAwesome 2";
 	}
 
 	public function setCondition($condition)
@@ -54,23 +59,35 @@ class IWActions
 		$this->condition = $condition;
 	}
 
+	public function setImageWidth($imageWidth)
+	{
+		$this->imageWidth = $imageWidth;
+	}
+
+	public function setItemSpace($itemSpace)
+	{
+		$this->itemSpace = $itemSpace;
+	}
+
+	public function setText($textTrue, $textFalse = '')
+	{
+		$this->item[0] = $textTrue;
+		$this->item[1] = $textFalse;
+	}
+
 	public function close()
 	{
 		$this->tollBar[$this->id] = $this->item;
-		$this->item = [];
-		$this->image = Null;
-		$this->fontAweSome = NULL;
-		$this->text = NULL;
-		$this->scImage = NULL;
 		$this->id++;
 	}
 
-	public function createToolBars()
+	public function createToolBar()
 	{
 		$html = '';
-		foreach ($this->tollBar as $key => $value) {
-			if ($this->condition) $html .= $value[0];
-			else $html.= $value[1]
+		//echo "CreateToobar<br>";
+		foreach ($this->tollBar as $value) {
+			//print_r($value);
+			$html .= ($this->condition) ? $value[0] : $value[1];
 		}
 		return $html;
 	}
